895677a5c0b84b04e57cb74d9c5d363b3f7a8092
Paul Lin <paul415236@gmail.com>
2018-04-01 18:41:10 +0800

    read paramters during insert module

--- m_hello/hello.c
+++ m_hello/hello.c
@@ -3,15 +3,22 @@
 #include <asm/current.h>
 #include <linux/sched.h>
 
+static char *name = "Paul";
+static int old = 18;
+
+module_param(name, charp, S_IRUGO);
+module_param(old, int, S_IRUGO);
+
 static int __init hello_int(void)
 {
-	printk("KERN_INFO hello %s: pid=%d \n", current->comm, current->pid);
+	printk("KERN_INFO %s: pid=%d \n", current->comm, current->pid);
+	printk("Hello %s(%d) \n", name, old);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk("KERN_INFO bye \n");
+	printk("KERN_INFO bye %s \n", name);
 }
 
 module_init(hello_int);
